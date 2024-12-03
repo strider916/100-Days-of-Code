69bcce1cd5ea220c3e8ebca2d3e94fa208fabac6
Trevor <trevorbarmann@gmail.com>
2024-12-03 13:43:20 -0800

    Day_25

---
+++ Day_25/main.py
@@ -0,0 +1,20 @@
+# with open("weather_data.csv", mode="r") as data:
+#      my_list = data.readlines()
+# print(my_list)
+#
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#
+#     print(temperatures)
+
+import pandas
+
+
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
