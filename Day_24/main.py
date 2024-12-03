455cbb244eae066dc2f0d96118db2ade0cea5492
Trevor <trevorbarmann@gmail.com>
2024-12-03 13:09:41 -0800

    Day_23 FINAL

--- Day_24/main.py
+++ Day_24/main.py
@@ -1,10 +1,11 @@
-with open("my_file.txt", mode="r") as file:
-    content = file.read()
-    print(content)
+PLACEHOLDER = "[name]"
 
-with open("my_file.txt", mode="a") as file:
-    file.write("\nHello, new world!")
-
-with open("my_file.txt", mode="r") as file:
-    content = file.read()
-    print(content)
+# Open file with all the names
+with open("names.txt", mode="r") as names_file:
+    # for every name in the name file (each on their own line)
+    for name in names_file.read().splitlines():
+        # Open the template letter
+        with open("starting_letter.txt", mode="r") as starting_letter:
+            # create a new file while replacing the placeholder text with the name
+            with open(f"./ReadyToSend/letter_for_{name}.txt", mode="w") as new_letter:
+                new_letter.write(starting_letter.read().replace(PLACEHOLDER, name))
