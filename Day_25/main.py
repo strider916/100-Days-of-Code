7b23ada93fa741c4c16baf10074545b9c47b8af5
Trevor <trevorbarmann@gmail.com>
2024-12-05 21:00:19 -0800

    Day_25

--- Day_25/main.py
+++ Day_25/main.py
@@ -41,6 +41,16 @@ import pandas
 
 
 data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20241203.csv")
-data.rename(columns={"Primary Fur Color": "fur_color"}, inplace=True)
+gray_squirrel_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrel_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrel_count = len(data[data["Primary Fur Color"] == "Black"])
 
-print(data.fur_color == "Gray")
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrel_count, red_squirrel_count, black_squirrel_count]
+}
+
+df = pandas.DataFrame(data_dict)
+
+df.to_csv("squirrel_count.csv")
+
